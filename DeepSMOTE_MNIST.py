27926414986c79a27a7bed25cf3f4a3cdc1b19a3
dd1github <42703897+dd1github@users.noreply.github.com>
2021-03-05 16:52:03 -0500

    Add files via upload

---
+++ DeepSMOTE_MNIST.py
@@ -0,0 +1,374 @@
+# -*- coding: utf-8 -*-
+
+import collections
+import torch
+import torch.nn as nn
+from torch.utils.data import TensorDataset
+import numpy as np
+from sklearn.neighbors import NearestNeighbors
+import time
+import os
+
+print(torch.version.cuda) #10.1
+t3 = time.time()
+##############################################################################
+"""args for AE"""
+
+args = {}
+args['dim_h'] = 64          # factor controlling size of hidden layers
+args['n_channel'] = 1#3       # number of channels in the input data 
+
+args['n_z'] = 300 #600     # number of dimensions in latent space. 
+
+args['sigma'] = 1.0        # variance in n_z
+args['lambda'] = 0.01      # hyper param for weight of discriminator loss
+args['lr'] = 0.0002        # learning rate for Adam optimizer .000
+args['epochs'] = 1 #50         # how many epochs to run for
+args['batch_size'] = 100   # batch size for SGD
+args['save'] = True        # save weights at each epoch of training if True
+args['train'] = True       # train networks if True, else load networks from
+
+args['dataset'] = 'mnist' #'fmnist' # specify which dataset to use
+
+
+##############################################################################
+
+
+
+## create encoder model and decoder model
+class Encoder(nn.Module):
+    def __init__(self, args):
+        super(Encoder, self).__init__()
+
+        self.n_channel = args['n_channel']
+        self.dim_h = args['dim_h']
+        self.n_z = args['n_z']
+        
+        # convolutional filters, work excellent with image data
+        self.conv = nn.Sequential(
+            nn.Conv2d(self.n_channel, self.dim_h, 4, 2, 1, bias=False),
+            #nn.ReLU(True),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(self.dim_h, self.dim_h * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(self.dim_h * 2),
+            #nn.ReLU(True),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(self.dim_h * 2, self.dim_h * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(self.dim_h * 4),
+            #nn.ReLU(True),
+            nn.LeakyReLU(0.2, inplace=True),
+            
+            
+            nn.Conv2d(self.dim_h * 4, self.dim_h * 8, 4, 2, 1, bias=False),
+            
+            #3d and 32 by 32
+            #nn.Conv2d(self.dim_h * 4, self.dim_h * 8, 4, 1, 0, bias=False),
+            
+            nn.BatchNorm2d(self.dim_h * 8), # 40 X 8 = 320
+            #nn.ReLU(True),
+            nn.LeakyReLU(0.2, inplace=True) )#,
+            #nn.Conv2d(self.dim_h * 8, 1, 2, 1, 0, bias=False))
+            #nn.Conv2d(self.dim_h * 8, 1, 4, 1, 0, bias=False))
+        # final layer is fully connected
+        self.fc = nn.Linear(self.dim_h * (2 ** 3), self.n_z)
+        
+
+    def forward(self, x):
+        #print('enc')
+        #print('input ',x.size()) #torch.Size([100, 3,32,32])
+        x = self.conv(x)
+        
+        x = x.squeeze()
+        #print('aft squeeze ',x.size()) #torch.Size([128, 320])
+        #aft squeeze  torch.Size([100, 320])
+        x = self.fc(x)
+        #print('out ',x.size()) #torch.Size([128, 20])
+        #out  torch.Size([100, 300])
+        return x
+
+
+class Decoder(nn.Module):
+    def __init__(self, args):
+        super(Decoder, self).__init__()
+
+        self.n_channel = args['n_channel']
+        self.dim_h = args['dim_h']
+        self.n_z = args['n_z']
+
+        # first layer is fully connected
+        self.fc = nn.Sequential(
+            nn.Linear(self.n_z, self.dim_h * 8 * 7 * 7),
+            nn.ReLU())
+
+        # deconvolutional filters, essentially inverse of convolutional filters
+        self.deconv = nn.Sequential(
+            nn.ConvTranspose2d(self.dim_h * 8, self.dim_h * 4, 4),
+            nn.BatchNorm2d(self.dim_h * 4),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(self.dim_h * 4, self.dim_h * 2, 4),
+            nn.BatchNorm2d(self.dim_h * 2),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(self.dim_h * 2, 1, 4, stride=2),
+            #nn.Sigmoid())
+            nn.Tanh())
+
+    def forward(self, x):
+        #print('dec')
+        #print('input ',x.size())
+        x = self.fc(x)
+        x = x.view(-1, self.dim_h * 8, 7, 7)
+        x = self.deconv(x)
+        return x
+
+##############################################################################
+"""set models, loss functions"""
+# control which parameters are frozen / free for optimization
+def free_params(module: nn.Module):
+    for p in module.parameters():
+        p.requires_grad = True
+
+def frozen_params(module: nn.Module):
+    for p in module.parameters():
+        p.requires_grad = False
+
+
+##############################################################################
+"""functions to create SMOTE images"""
+
+def biased_get_class(c):
+    
+    xbeg = dec_x[dec_y == c]
+    ybeg = dec_y[dec_y == c]
+    
+    return xbeg, ybeg
+    #return xclass, yclass
+
+
+def G_SM(X, y,n_to_sample,cl):
+
+    # determining the number of samples to generate
+    #n_to_sample = 10 
+
+    # fitting the model
+    n_neigh = 5 + 1
+    nn = NearestNeighbors(n_neighbors=n_neigh, n_jobs=1)
+    nn.fit(X)
+    dist, ind = nn.kneighbors(X)
+
+    # generating samples
+    base_indices = np.random.choice(list(range(len(X))),n_to_sample)
+    neighbor_indices = np.random.choice(list(range(1, n_neigh)),n_to_sample)
+
+    X_base = X[base_indices]
+    X_neighbor = X[ind[base_indices, neighbor_indices]]
+
+    samples = X_base + np.multiply(np.random.rand(n_to_sample,1),
+            X_neighbor - X_base)
+
+    #use 10 as label because 0 to 9 real classes and 1 fake/smoted = 10
+    return samples, [cl]*n_to_sample
+
+#xsamp, ysamp = SM(xclass,yclass)
+
+###############################################################################
+
+dtrnimg = '.../MNIST/trn_img/'
+dtrnlab = '...MNIST/trn_lab/'
+
+ids = os.listdir(dtrnimg)
+idtri_f = [os.path.join(dtrnimg, image_id) for image_id in ids]
+print(idtri_f)
+
+ids = os.listdir(dtrnlab)
+idtrl_f = [os.path.join(dtrnlab, image_id) for image_id in ids]
+print(idtrl_f)
+
+for i in range(5):
+    print()
+    print(i)
+    encoder = Encoder(args)
+    decoder = Decoder(args)
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print(device)
+    decoder = decoder.to(device)
+    encoder = encoder.to(device)
+
+    train_on_gpu = torch.cuda.is_available()
+
+    #decoder loss function
+    criterion = nn.MSELoss()
+    criterion = criterion.to(device)
+    
+    trnimgfile = idtri_f[i]
+    trnlabfile = idtrl_f[i]
+    
+    print(trnimgfile)
+    print(trnlabfile)
+    dec_x = np.loadtxt(trnimgfile) 
+    dec_y = np.loadtxt(trnlabfile)
+
+    print('train imgs before reshape ',dec_x.shape) #(39993, 784)
+    print('train labels ',dec_y.shape) #(39993,)
+    print(collections.Counter(dec_y))
+    dec_x = dec_x.reshape(dec_x.shape[0],1,28,28)   
+    print('train imgs after reshape ',dec_x.shape) #(39993,1,28,28)
+
+    batch_size = 100
+    num_workers = 0
+
+    #torch.Tensor returns float so if want long then use torch.tensor
+    tensor_x = torch.Tensor(dec_x)#,dtype=torch.long,device=device) # transform to torch tensor
+    tensor_y = torch.tensor(dec_y,dtype=torch.long)#,device=device)
+    mnist_bal = TensorDataset(tensor_x,tensor_y) # create your datset
+
+    train_loader = torch.utils.data.DataLoader(mnist_bal, 
+        batch_size=batch_size,shuffle=True,num_workers=num_workers)
+    
+    classes = ('0', '1', '2', '3', '4',
+           '5', '6', '7', '8', '9')
+
+    best_loss = np.inf
+
+    args['epochs'] = 200 
+
+    t0 = time.time()
+    if args['train']:
+        enc_optim = torch.optim.Adam(encoder.parameters(), lr = args['lr'])
+        dec_optim = torch.optim.Adam(decoder.parameters(), lr = args['lr'])
+    
+        for epoch in range(args['epochs']):
+            train_loss = 0.0
+            tmse_loss = 0.0
+            tdiscr_loss = 0.0
+            # train for one epoch -- set nets to train mode
+            encoder.train()
+            decoder.train()
+        
+            for images,labs in train_loader:
+                #for images,labs in train_loadtst:
+                #for images, labs in tqdm(train_loader):
+            
+                # zero gradients for each batch
+                encoder.zero_grad()
+                decoder.zero_grad()
+                #print(images)
+                images, labs = images.to(device), labs.to(device)
+                #print('images ',images.size()) #torch.Size([100, 3, 32, 32])
+                labsn = labs.detach().cpu().numpy()
+                #print('labsn ',labsn.shape, labsn)
+            
+                # run images
+                z_hat = encoder(images)
+            
+                x_hat = decoder(z_hat) #decoder outputs tanh
+                #print('xhat ', x_hat.size())
+                #print(x_hat)
+                mse = criterion(x_hat,images)
+                #print('mse ',mse)
+                #tensor(0.6623, device='cuda:0', grad_fn=<MeanBackward0>)
+                       
+                resx = []
+                resy = []
+            
+                #for t in range(10):
+                tc = np.random.choice(10,1)
+                #tc = 9
+                xbeg = dec_x[dec_y == tc]
+                ybeg = dec_y[dec_y == tc] 
+                xlen = len(xbeg)
+                nsamp = min(xlen, 100)
+                ind = np.random.choice(list(range(len(xbeg))),nsamp,replace=False)
+                xclass = xbeg[ind]
+                yclass = ybeg[ind]
+            
+                xclen = len(xclass)
+                #print('xclen ',xclen)
+                xcminus = np.arange(1,xclen)
+                #print('minus ',xcminus.shape,xcminus)
+                #array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+                xcplus = np.append(xcminus,0)
+                #print('xcplus ',xcplus)
+                xcnew = (xclass[[xcplus],:])
+                #xcnew = np.squeeze(xcnew)
+                xcnew = xcnew.reshape(xcnew.shape[1],xcnew.shape[2],xcnew.shape[3],xcnew.shape[4])
+                #print('xcnew ',xcnew.shape)
+            
+                xcnew = torch.Tensor(xcnew)
+                xcnew = xcnew.to(device)
+            
+                #encode xclass to feature space
+                xclass = torch.Tensor(xclass)
+                xclass = xclass.to(device)
+                xclass = encoder(xclass)
+                #print('xclass ',xclass.shape) #torch.Size([100, 600])
+            
+                xclass = xclass.detach().cpu().numpy()
+            
+                #print('xcplus2 ',xcplus)
+                xc_enc = (xclass[[xcplus],:])
+                xc_enc = np.squeeze(xc_enc)
+                #print('xc enc ',xc_enc.shape)
+            
+                xc_enc = torch.Tensor(xc_enc)
+                xc_enc = xc_enc.to(device)
+            
+                #xc_enc = xc_enc.view(xc_enc.size()[0], xc_enc.size()[1], 1, 1)
+                
+                ximg = decoder(xc_enc)
+                
+                mse2 = criterion(ximg,xcnew)
+            
+                comb_loss = mse2 + mse
+                comb_loss.backward()
+            
+                enc_optim.step()
+                dec_optim.step()
+            
+                train_loss += comb_loss.item()*images.size(0)
+                tmse_loss += mse.item()*images.size(0)
+                tdiscr_loss += mse2.item()*images.size(0)
+            
+                 
+            # print avg training statistics 
+            train_loss = train_loss/len(train_loader)
+            tmse_loss = tmse_loss/len(train_loader)
+            tdiscr_loss = tdiscr_loss/len(train_loader)
+            print('Epoch: {} \tTrain Loss: {:.6f} \tmse loss: {:.6f} \tmse2 loss: {:.6f}'.format(epoch,
+                    train_loss,tmse_loss,tdiscr_loss))
+            
+        
+            if train_loss < best_loss:
+                print('Saving..')
+                path_enc = '.../MNIST/models/crs5/' \
+                    + str(i) + '/bst_enc.pth'
+                path_dec = '.../MNIST/models/crs5/' \
+                    + str(i) + '/bst_dec.pth'
+             
+                torch.save(encoder.state_dict(), path_enc)
+                torch.save(decoder.state_dict(), path_dec)
+        
+                best_loss = train_loss
+        
+        
+        #c = str(classname)
+        path_enc = '.../MNIST/models/crs5/' \
+            + str(i) + '/f_enc.pth'
+        path_dec = '.../MNIST/models/crs5/' \
+            + str(i) + '/f_dec.pth'
+        print(path_enc)
+        print(path_dec)
+        torch.save(encoder.state_dict(), path_enc)
+        torch.save(decoder.state_dict(), path_dec)
+        print()
+              
+    t1 = time.time()
+    print('total time(min): {:.2f}'.format((t1 - t0)/60))             
+ 
+t4 = time.time()
+print('final time(min): {:.2f}'.format((t4 - t3)/60))
+
+
+
+
+
